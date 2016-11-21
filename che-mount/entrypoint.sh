9696faad4ab5c41b73a3e09326dbe9585fe5efd4
Florent BENOIT <fbenoit@codenvy.com>
2016-11-21 15:37:18 +0100

    fixup! rework docker containers to be compliant with "single folder self-contained" - each files used by Dockerfile are inside the folder - refactor lib/typescript and lib/maven-dto to be a single docker image - add inheritance for cheddar, cha-action and che-test - each docker entry point script is now called entrypoint.sh - build.sh for each single directory (+ build-all.sh script at root folder to build all images)
    Change-Id: I7a91ff29f6e1d5644f40efe25d2dea36715a27be
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- che-mount/entrypoint.sh
+++ che-mount/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright (c) 2016 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
