699f65d79ec655187b77328c92c201bf4c9bb6f6
Florent BENOIT <fbenoit@codenvy.com>
2016-11-21 14:26:38 +0100

    rework docker containers to be compliant with "single folder self-contained" - each files used by Dockerfile are inside the folder - refactor lib/typescript and lib/maven-dto to be a single docker image - add inheritance for cheddar, cha-action and che-test - each docker entry point script is now called entrypoint.sh - build.sh for each single directory (+ build-all.sh script at root folder to build all images)
    Change-Id: I9d77157d9232e9df9be0f363f1ba1374df6585dd
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- lib/maven-dto/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DIR=$(cd "$(dirname "$0")"; pwd)
-echo "Generating TypeScript DTO from $DIR directory"
-cd $DIR
-rm -rf target
-docker run -i --rm -v "$HOME/.m2:/root/.m2" -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 /bin/bash -c "groupadd user && useradd -g user user && (chown --silent -R user.user /usr/src/app || true) && mvn -DskipTests=true -Dfindbugs.skip=true -Dskip-validate-sources install && (chown --silent -R user.user /usr/src/app || true)"
-mkdir -p ../typescript/src/api/dto
-cp target/dto-typescript.ts ../typescript/src/api/dto/che-dto.ts
