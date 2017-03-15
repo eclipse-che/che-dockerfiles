afcfd0c82a80410348f02c46de8b8b680b8b6a28
Gytis Trikleris <gytis@redhat.com>
2017-03-15 09:46:47 +0100

    Spring Boot recipe
    Signed-off-by: Gytis Trikleris <gytis@redhat.com>

---
+++ recipes/centos_spring_boot/install_spring_boot_dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Copyright (c) 2012-2017 Red Hat, Inc
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+
+function install {
+  mvn dependency:get -Dartifact=$1 > /tmp/maven-$1.log
+  if [ $? -eq 0 ]; then
+    echo -e "🍻  ${YELLOW}$1 installed successfully ${NC}"
+  else
+    echo -e "☠️  ${RED}Unable to install $1 ${NC}"
+    cat /tmp/maven-$1.log
+    exit 1
+  fi
+}
+
+echo -e "${BLUE}Installing Spring Boot ${SPRING_BOOT_VERSION} dependencies..."
+install $SPRING_BOOT_GROUP:spring-boot-starter-parent:$SPRING_BOOT_VERSION:pom
+install $SPRING_BOOT_GROUP:spring-boot-starter-web:$SPRING_BOOT_VERSION
+install $SPRING_BOOT_GROUP:spring-boot-starter-test:$SPRING_BOOT_VERSION
+install $SPRING_BOOT_GROUP:spring-boot-loader-tools:$SPRING_BOOT_VERSION
+install $SPRING_BOOT_GROUP:spring-boot-maven-plugin:$SPRING_BOOT_VERSION
+install $SPRING_BOOT_GROUP:spring-boot-tools:$SPRING_BOOT_VERSION:pom
+install junit:junit:$JUNIT_VERSION
+echo -e "${BLUE}Spring Boot ${SPRING_BOOT_VERSION} dependencies installed ${NC}"
+rm -Rf /tmp/maven-*
