8a624b91794bd2f21e1b5c5893a892ef1b770bcc
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-10-16 19:05:16 +0530

    Added infinite loop to run.sh
    Refactored Dockerfile for some commands on RUN to be merged
    
    Signed-off-by: Kamesh Sampath <kamesh.sampath@hotmail.com>

--- recipes/centos_wildfly_swarm/run.sh
+++ recipes/centos_wildfly_swarm/run.sh
@@ -8,16 +8,18 @@
 set -e
 
 # insert settings for mirrors/repository managers into settings.xml if supplied
-function configure_mirrors {
+configure_mirrors() {
   if [ -n "$MAVEN_MIRROR_URL" ]; then
     xml="    <mirror>\
       <id>mirror.default</id>\
       <url>$MAVEN_MIRROR_URL</url>\
       <mirrorOf>external:*</mirrorOf>\
     </mirror>"
-    sed -i "s|<!-- ### configured mirrors ### -->|$xml|" $HOME/.m2/settings.xml
+    sed -i "s|<!-- ### configured mirrors ### -->|$xml|" "{$HOME}/.m2/settings.xml"
   fi
 }
 
 # check for the MAVEN_MIRROR_URL env variable, if its available then set maven mirrors in $HOME/.m2/settings.xml
 configure_mirrors
+
+while :; do sleep 1; done
