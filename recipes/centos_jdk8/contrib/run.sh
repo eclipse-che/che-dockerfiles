36c510e96f92e1a3298b66e604e5f3707ec5eb91
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-10-17 10:50:57 +0530

    Improvements:  - removed redundant run.sh from all images using centos-jdk8 as base  - add run.sh to centos-jdk8 base image  - overide the CMD of centos-jdk8 to use run.sh to allow maven settings
    Signed-off-by: Kamesh Sampath <kamesh.sampath@hotmail.com>

--- recipes/centos_jdk8/contrib/run.sh
+++ recipes/centos_jdk8/contrib/run.sh
@@ -1,25 +1,29 @@
-#!/bin/sh
-# Copyright (c) 2012-2017 Red Hat, Inc
+#!/bin/bash
+# Copyright (c) 2012-2017 Red Hat, Inc.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
+# Contributors:
+# Red Hat, Inc. - initial implementation
 
 set -e
 
 # insert settings for mirrors/repository managers into settings.xml if supplied
 configure_mirrors() {
+  echo "Setting maven mirror from $MAVEN_MIRROR_URL"
   if [ -n "$MAVEN_MIRROR_URL" ]; then
     xml="    <mirror>\
       <id>mirror.default</id>\
       <url>$MAVEN_MIRROR_URL</url>\
       <mirrorOf>external:*</mirrorOf>\
     </mirror>"
-    sed -i "s|<!-- ### configured mirrors ### -->|$xml|" "{$HOME}/.m2/settings.xml"
+    sed -i "s|<!-- ### configured mirrors ### -->|$xml|" "${HOME}/.m2/settings.xml"
   fi
 }
 
 # check for the MAVEN_MIRROR_URL env variable, if its available then set maven mirrors in $HOME/.m2/settings.xml
+
 configure_mirrors
 
-exec tail -f /dev/null
+tail -f /dev/null
