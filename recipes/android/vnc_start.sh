9b80a0fb828b9ddbe8329479530dd948e272ca46
Eugene Ivantsov <eivantsov@redhat.com>
2018-06-05 11:21:25 +0300

    Centos based Android image

---
+++ recipes/android/vnc_start.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Copyright (c) 2012-2018 Red Hat, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+# Contributors:
+# Red Hat, Inc. - initial implementation
+
+set -e
+
+if [[ $1 =~ -h|--help ]]; then
+    help
+    exit 0
+fi
+
+source $HOME/.bashrc
+if [[ $1 =~ -s|--skip ]]; then
+    echo -e "\n\n------------------ SKIP VNC STARTUP -----------------"
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '${@:2}'"
+    exec "${@:2}"
+fi
+if [[ $1 =~ -d|--debug ]]; then
+    echo -e "\n\n------------------ DEBUG VNC STARTUP -----------------"
+    export DEBUG=true
+fi
+
+cleanup () {
+    kill -s SIGTERM $!
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
+
+VNC_IP=$(hostname -i)
+
+echo -e "\n------------------ change VNC password  ------------------"
+mkdir -p "$HOME/.vnc"
+PASSWD_PATH="$HOME/.vnc/passwd"
+if [[ $VNC_VIEW_ONLY == "true" ]]; then
+    echo "start VNC server in VIEW ONLY mode!"
+    echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
+fi
+echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+chmod 600 $PASSWD_PATH
+
+echo -e "\n------------------ start noVNC  ----------------------------"
+if [[ $DEBUG == true ]]; then echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"; fi
+$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> $STARTUPDIR/no_vnc_startup.log &
+PID_SUB=$!
+
+echo -e "\n------------------ start VNC server ------------------------"
+echo "remove old vnc locks to be a reattachable container"
+vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
+    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $STARTUPDIR/vnc_startup.log \
+    || echo "no locks present"
+
+echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
+if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION &> $STARTUPDIR/no_vnc_startup.log
+echo -e "start window manager\n..."
+
+xset -dpms &
+xset s noblank &
+xset s off &
+
+/usr/bin/startxfce4 --replace > $HOME/wm.log &
+sleep 1
+cat $HOME/wm.log
+
+echo -e "\n\n------------------ VNC environment started ------------------"
+echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
+echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
+
+
+if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
+    echo -e "\n------------------ $HOME/.vnc/*$DISPLAY.log ------------------"
+    tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
+fi
+
+if [ -z "$1" ] || [[ $1 =~ -w|--wait ]]; then
+    wait $PID_SUB
+else
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '$@'"
+    exec "$@"
+fi
