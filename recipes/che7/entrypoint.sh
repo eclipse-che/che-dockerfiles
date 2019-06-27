a21699568273b8924ef55355bba1ba954b90a7a2
Angel Misevski <amisevsk@redhat.com>
2019-06-27 04:18:46 -0400

    che #13454 Support arbitrary user in che 7 images
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ recipes/che7/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+  fi
+fi
+
+exec "$@"
